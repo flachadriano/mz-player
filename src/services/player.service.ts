d06e444e54afdce498f336596803320272f6bc7b
Adriano Flach de Araujo <adriano.spintec@oncologiador.corp>
2024-03-27 19:03:17 -0300

    feat: adding canvas image

--- src/services/player.service.ts
+++ src/services/player.service.ts
@@ -2,7 +2,24 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PlayerService {
+
   getImage(): string {
-    return 'new image';
+    const
+        fs = require("fs"),
+        { createCanvas, loadImage } = require("canvas");
+
+    const WIDTH = 100;
+    const HEIGHT = 50;
+
+    const canvas = createCanvas(WIDTH, HEIGHT);
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "#222222";
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    ctx.fillStyle = "#f2f2f2";
+    ctx.font = "32px Arial";
+    ctx.fillText("Hello", 13, 35);
+
+    return canvas.toDataURL();
   }
 }
